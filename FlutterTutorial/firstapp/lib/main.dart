aa88452f9ee759629092e7d3e8efc57207938b19
ThuktenSingye <thuktensingye2163@gmail.com>
2023-10-08 16:49:33 +0600

    added different button

--- FlutterTutorial/firstapp/lib/main.dart
+++ FlutterTutorial/firstapp/lib/main.dart
@@ -51,11 +51,50 @@ class Home extends StatelessWidget {
         // child : Image.asset('assets/ddc.jpg'),
         // child: Image.network('https://images.unsplash.com/photo-1516339901601-2e1b62dc0c45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1871&q=80'),
         // Icons
-        child: Icon(
-          Icons.airport_shuttle,
-          color: Colors.lightBlue,
-          size: 50.0,
-        ),
+        // child: Icon(
+        //   Icons.airport_shuttle,
+        //   color: Colors.lightBlue,
+        //   size: 50.0,
+        // ),
+        // Buttons
+        // there are other button like TextButton, OutlinedButton, IconButton, FloatingActionButton and dropdownButton
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              ElevatedButton.icon(
+                onPressed: () {
+                  print('Mail Send');
+                },
+                icon: Icon(
+                  Icons.mail,
+                ),
+                label: Text('Mail Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  elevation: MaterialStateProperty.all<double>(8.0), // Set the elevation (shadow)
+                  textStyle: MaterialStateProperty.all<TextStyle>(
+                    TextStyle(fontSize: 18.0), // Set the text style
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: (){
+                  print('You click on text button');
+                }, 
+                child: Text('Text Button')
+              ),
+              IconButton(
+                onPressed: (){
+                  print('You click on icon button');
+                  }, 
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.red[300],
+                ),
+                ),
+            ],
+          ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
