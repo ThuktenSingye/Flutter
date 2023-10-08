9925b6cd17dad76c14dab1445eee6e0d0b1e80c5
ThuktenSingye <thuktensingye2163@gmail.com>
2023-10-08 17:28:16 +0600

    container # padding # margin

--- FlutterTutorial/firstapp/lib/main.dart
+++ FlutterTutorial/firstapp/lib/main.dart
@@ -28,74 +28,22 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        // child: Text(
-        //   'Hello World',
-        //   style: TextStyle(
-        //     fontSize: 20.0,
-        //     fontWeight: FontWeight.bold,
-        //     letterSpacing: 2.0,
-        //     color: Colors.grey,
-        //     fontFamily: 'IndieFlower'
 
-        //   ),
-        // ),
-        // Images
-        // child: Image(
-        //   image: NetworkImage('https://images.unsplash.com/photo-1516339901601-2e1b62dc0c45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1871&q=80')
-        // ),Network Image
-        // child: Image(
-        //   image:AssetImage('assets/ddc.jpg')
-        // ),
-        // the shortcut to above code is given below
-        // child : Image.asset('assets/ddc.jpg'),
-        // child: Image.network('https://images.unsplash.com/photo-1516339901601-2e1b62dc0c45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1871&q=80'),
-        // Icons
-        // child: Icon(
-        //   Icons.airport_shuttle,
-        //   color: Colors.lightBlue,
-        //   size: 50.0,
-        // ),
-        // Buttons
-        // there are other button like TextButton, OutlinedButton, IconButton, FloatingActionButton and dropdownButton
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton.icon(
-                onPressed: () {
-                  print('Mail Send');
-                },
-                icon: Icon(
-                  Icons.mail,
-                ),
-                label: Text('Mail Me'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                  elevation: MaterialStateProperty.all<double>(8.0), // Set the elevation (shadow)
-                  textStyle: MaterialStateProperty.all<TextStyle>(
-                    TextStyle(fontSize: 18.0), // Set the text style
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: (){
-                  print('You click on text button');
-                }, 
-                child: Text('Text Button')
-              ),
-              IconButton(
-                onPressed: (){
-                  print('You click on icon button');
-                  }, 
-                icon: Icon(
-                  Icons.home,
-                  color: Colors.red[300],
-                ),
-                ),
-            ],
-          ),
-        )
+      // if you want only padding you can use padding widget else container if you want both padding and margin
+      // body:Container(
+      //   // EdgeInsets.all() all padding
+      //   // symmetric(ver, hor)  vertical padding, horizontal padding
+      //   // fromLTRB(l, t, r, b) left, top, right, bottom // same case as margin
+      //   padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+      //   color: Colors.grey[400],
+      //   margin: EdgeInsets.all(20.0),
+      //   child: Text('Hello'),
+      // ),
+      body: Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text('hello world'),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
