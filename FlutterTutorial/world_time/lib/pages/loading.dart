5d178230c21e4077160285bd3760eeefdb0db414
ThuktenSingye <thuktensingye2163@gmail.com>
2023-10-10 10:57:36 +0600

    Flutter package dependencies

--- FlutterTutorial/world_time/lib/pages/loading.dart
+++ FlutterTutorial/world_time/lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // used to convert json string to string
 class Loading extends StatefulWidget {
   const Loading({super.key});
 
@@ -8,6 +9,23 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async { // this function is asyncr
+
+    Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    http.Response response = await http.get(url);
+    Map data = jsonDecode(response.body);
+    print('user id: ${data['userId']}');
+
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState(); // calling parents initstate function
+    getData();
+  
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
